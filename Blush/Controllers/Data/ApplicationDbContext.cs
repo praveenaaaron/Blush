0c9502675013ca806a7766159e05e986455792cf
praveenaaaron <piravienamca12@gmail.com>
2024-06-08 11:54:00 +0530

    layoutfixed and connection string setsuccessfully

---
+++ Blush/Controllers/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Blush.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Blush.Controllers.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                  new Category { Id = 1, Name = "Fackpack", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Lipstick", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Foundation", DisplayOrder = 3 }
+                );
+        }
+    }
+}
