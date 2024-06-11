f3e1b5f0224f666ece2fd2f3e228a65406e7a0e9
praveenaaaron <piravienamca12@gmail.com>
2024-06-11 21:54:13 +0530

    Create db

---
+++ Blush/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Blush.Controllers.Data;
+using Blush.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blush.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
