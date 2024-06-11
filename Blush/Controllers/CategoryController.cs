31c6db1fa368115f46a005544585dd80a89b4d25
praveenaaaron <piravienamca12@gmail.com>
2024-06-11 18:54:55 +0530

    CURD operation on Category

---
+++ Blush/Controllers/CategoryController.cs
@@ -0,0 +1,86 @@
+﻿
+using Blush.Controllers.Data;
+using Blush.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace Blush.Controllers
+{
+    public class CategoryController:Controller
+    {
+      private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            TempData["Success"] = "Category Created Successfully";
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOSST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["Success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+
+
+        }
+    }
+}
